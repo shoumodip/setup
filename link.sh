97979a09101d09419de4946550e1a5dbfe6f1f4f
Shoumodip Kar <shoumodipkar@gmail.com>
2024-07-15 20:04:13 +0530

    Switch back to `xfce4-terminal`

--- link.sh
+++ link.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 link() {
-  mkdir -p $2
-  ln -sf $PWD/src/$1 $2
+    mkdir -p $2
+    ln -sf $PWD/src/$1 $2
 }
 
 link .bashrc ~/
 link .gitconfig ~/
 link .clang-format ~/
 link init.lua ~/.config/nvim/
-link tmux.conf ~/.config/tmux/
-link alacritty.yml ~/.config/alacritty/
+link gruvbox-material.theme ~/.local/share/xfce4/terminal/colorschemes/
--- src/init.lua
+++ src/init.lua
@@ -33,35 +33,35 @@ local paq_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
 local paq_installed = vim.fn.empty(vim.fn.glob(paq_path)) == 0
 
 if not paq_installed then
-  print("Installing plugins...")
-  vim.fn.system({"git", "clone", "--depth=1", "https://github.com/savq/paq-nvim", paq_path})
-  vim.cmd("packadd paq-nvim")
-  vim.cmd("autocmd User PaqDoneInstall quit")
+    print("Installing plugins...")
+    vim.fn.system({"git", "clone", "--depth=1", "https://github.com/savq/paq-nvim", paq_path})
+    vim.cmd("packadd paq-nvim")
+    vim.cmd("autocmd User PaqDoneInstall quit")
 end
 
 require("paq") {
-  "savq/paq-nvim",
-  "shoumodip/ido.nvim",
-  "shoumodip/compile.nvim",
-  "sainnhe/gruvbox-material",
-  "nvim-treesitter/nvim-treesitter",
+    "savq/paq-nvim",
+    "shoumodip/ido.nvim",
+    "shoumodip/compile.nvim",
+    "sainnhe/gruvbox-material",
+    "nvim-treesitter/nvim-treesitter",
 
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
 
-  "tpope/vim-rsi",
-  "tpope/vim-repeat",
-  "tpope/vim-fugitive",
-  "tpope/vim-surround",
-  "tpope/vim-commentary"
+    "tpope/vim-rsi",
+    "tpope/vim-repeat",
+    "tpope/vim-fugitive",
+    "tpope/vim-surround",
+    "tpope/vim-commentary"
 }
 
 if not paq_installed then
-  vim.cmd("PaqInstall")
-  return
+    vim.cmd("PaqInstall")
+    return
 end
 
 vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
@@ -94,45 +94,45 @@ vim.keymap.set("n", "<leader>k", ":CompilePrev<cr>")
 vim.keymap.set("n", "<leader>m", ":Mason<cr>")
 
 vim.api.nvim_create_autocmd({"FileType"}, {
-  pattern = {"c", "cpp", "glsl"},
-  command = "setlocal commentstring=//%s",
+    pattern = {"c", "cpp", "glsl"},
+    command = "setlocal commentstring=//%s",
 })
 
 vim.api.nvim_create_autocmd({"FileType"}, {
-  pattern = {"go"},
-  command = "setlocal noexpandtab",
+    pattern = {"go"},
+    command = "setlocal noexpandtab",
 })
 
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = {"*"},
-  callback = function()
-    if pcall(vim.api.nvim_buf_get_var, 0, "lspformat") then
-      vim.lsp.buf.format()
-    else
-      local save = vim.fn.winsaveview()
-      vim.cmd("keeppatterns %s/\\s\\+$//e")
-      vim.cmd("keeppatterns %s/\\n\\+\\%$//e")
-      vim.fn.winrestview(save)
+    pattern = {"*"},
+    callback = function()
+        if pcall(vim.api.nvim_buf_get_var, 0, "lspformat") then
+            vim.lsp.buf.format()
+        else
+            local save = vim.fn.winsaveview()
+            vim.cmd("keeppatterns %s/\\s\\+$//e")
+            vim.cmd("keeppatterns %s/\\n\\+\\%$//e")
+            vim.fn.winrestview(save)
+        end
     end
-  end
 })
 
 require("nvim-treesitter.configs").setup {
-  indent = {
-    enable = true,
-    disable = {"c", "cpp"}
-  },
-
-  highlight = {enable = true},
-  auto_install = true,
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<tab>",
-      node_incremental = "<tab>",
-      node_decremental = "<s-tab>",
+    indent = {
+        enable = true,
+        disable = {"c", "cpp"}
     },
-  }
+
+    highlight = {enable = true},
+    auto_install = true,
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<tab>",
+            node_incremental = "<tab>",
+            node_decremental = "<s-tab>",
+        },
+    }
 }
 
 local ido = require("ido")
@@ -149,24 +149,24 @@ require("compile").bind {q = vim.cmd.close}
 
 local cmp = require("cmp")
 cmp.setup {
-  mapping = cmp.mapping.preset.insert {
-    ["<cr>"] = cmp.mapping.confirm {select = true},
-    ["<tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<s-tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end
-  },
-  sources = cmp.config.sources {{name = "nvim_lsp"}},
+    mapping = cmp.mapping.preset.insert {
+        ["<cr>"] = cmp.mapping.confirm {select = true},
+        ["<tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<s-tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end
+    },
+    sources = cmp.config.sources {{name = "nvim_lsp"}},
 }
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -174,24 +174,24 @@ capabilities.textDocument.completion.completionItem.snippetSupport = false
 
 require("mason").setup()
 require("mason-lspconfig").setup_handlers {
-  function (server)
-    require("lspconfig")[server].setup {
-      capabilities = capabilities,
-      on_attach = function (client, buffer)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = buffer})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = buffer})
-        vim.keymap.set("n", "gr", vim.lsp.buf.rename, {buffer = buffer})
-        vim.keymap.set("n", "<leader>l", vim.lsp.buf.references, {buffer = buffer})
-        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, {buffer = buffer})
-        vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next, {buffer = buffer})
-        vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_prev, {buffer = buffer})
+    function (server)
+        require("lspconfig")[server].setup {
+            capabilities = capabilities,
+            on_attach = function (client, buffer)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = buffer})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = buffer})
+                vim.keymap.set("n", "gr", vim.lsp.buf.rename, {buffer = buffer})
+                vim.keymap.set("n", "<leader>l", vim.lsp.buf.references, {buffer = buffer})
+                vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, {buffer = buffer})
+                vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next, {buffer = buffer})
+                vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_prev, {buffer = buffer})
 
-        if client.server_capabilities.documentFormattingProvider then
-          vim.api.nvim_buf_set_var(buffer, "lspformat", true)
-        end
-      end
-    }
-  end
+                if client.server_capabilities.documentFormattingProvider then
+                    vim.api.nvim_buf_set_var(buffer, "lspformat", true)
+                end
+            end
+        }
+    end
 }
 
 vim.diagnostic.config {update_in_insert = true}
