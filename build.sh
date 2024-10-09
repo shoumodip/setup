1cd2d4d1a2e0d0e92ed6f1ff4126a390e7bb18d5
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-09 14:57:19 +0530

    Make the user shell `sh`
    The terminal can launch `fish` as the main process

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo xbps-install neovim xclip bspwm sxhkd fish-shell base-devel fonts-roboto-ttf font-iosevka papirus-icon-theme xsetroot
-sudo chsh -s $(which fish) $USER
+sudo chsh -s $(which sh) $USER
 
 link() {
     mkdir -p $2
