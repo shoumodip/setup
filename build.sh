eeae8caf4bdd9d64c1c71b3b4d4235ce96ada4e7
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-12 12:52:55 +0530

    Switch to JetBrains Mono

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo xbps-install neovim xclip bspwm sxhkd fish-shell base-devel fonts-roboto-ttf font-iosevka papirus-icon-theme xsetroot feh slock
+sudo xbps-install neovim xclip bspwm sxhkd fish-shell base-devel fonts-roboto-ttf papirus-icon-theme xsetroot feh slock curl
 
 link() {
     mkdir -p $2
@@ -19,3 +19,8 @@ link config.fish ~/.config/fish/
 link fish_variables ~/.config/fish/
 link fish_prompt.fish ~/.config/fish/functions/
 link gruvbox-material.theme ~/.local/share/xfce4/terminal/colorschemes/
+
+FONT=~/.local/share/fonts/JetBrainsMono
+mkdir -p $FONT && cd $FONT
+curl -LO https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+unzip *.zip && rm *.zip
