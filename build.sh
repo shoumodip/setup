5022637b5720669819039ef008cfa573785fd624
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-12 19:46:38 +0530

    Add icons and themes

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Packages
 sudo xbps-install neovim xclip bspwm sxhkd fish-shell base-devel fonts-roboto-ttf papirus-icon-theme xsetroot feh slock curl
 
+# Dotfiles
 link() {
     mkdir -p $2
     ln -sf $PWD/src/$1 $2
@@ -18,8 +20,18 @@ link snippets.lua ~/.config/nvim/lua/
 link config.fish ~/.config/fish/
 link fish_variables ~/.config/fish/
 link fish_prompt.fish ~/.config/fish/functions/
+
+link settings.ini ~/.config/gtk-2.0/
+link settings.ini ~/.config/gtk-3.0/
 link gruvbox-material.theme ~/.local/share/xfce4/terminal/colorschemes/
 
+# Icons and Themes
+git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk --depth 1
+sudo mv gruvbox-material-gtk/icons/Gruvbox-Material-Dark /usr/share/icons/
+sudo mv gruvbox-material-gtk/themes/Gruvbox-Material-Dark /usr/share/themes/
+rm -rf gruvbox-material-gtk
+
+# Font
 FONT=~/.local/share/fonts/JetBrainsMono
 mkdir -p $FONT && cd $FONT
 curl -LO https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
